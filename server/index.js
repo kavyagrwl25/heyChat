a298b5d3400688d353e520113ac3907f0aecc0c5
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2026-05-02 11:55:27 +0530

    feat: integrate socket.io for real-time communication and update dependencies
    - Added socket.io-client to the client for real-time capabilities.
    - Created a basic Express server with socket.io for handling connections.
    - Removed unused CSS and components from the client for a cleaner structure.
    - Updated package.json files for both client and server to include necessary dependencies.
    - Added CORS support to the server for cross-origin requests.
    - Created initial server setup with a simple endpoint and connection logging.

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import { createServer } from "http";
+import { Server } from 'socket.io';
+import cors from "cors";
+
+const app = express();
+const PORT = 4000;
+const server = createServer(app);
+const io = new Server(server,{
+    cors: {
+        origin: "http://localhost:5173",
+        credentials: true
+    }
+});          
+
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`)
+})
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+server.listen(PORT, ()=> {
+    console.log(`You are now connected on server at ${PORT}`)
+})
